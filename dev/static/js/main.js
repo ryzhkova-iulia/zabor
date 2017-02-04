c9f2e48df0d19bd6fdc68921102078bc3ad6187b
Anton Zhelonkin <basurlakh@gmail.com>
2017-02-04 16:14:54 +0300

    finished all except scripts

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,2 +1,22 @@
 ;
 // Начинать писать отсюда!!!!
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+            center: [55.864005, 37.677292],
+            zoom: 16
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        myPlacemark = new ymaps.Placemark([37.677292, 37.677292], {
+            // Чтобы балун и хинт открывались на метке, необходимо задать ей определенные свойства.
+            // balloonContentHeader: "Рольф Химки",
+            balloonContentBody: "г. Москва, ул. Коминтерна, д.7, к.2",
+            // balloonContentFooter: "Подвал",
+        });
+
+    myMap.geoObjects.add(myPlacemark);
+    myMap.behaviors.disable('scrollZoom');
+    myMap.controls.remove('trafficControl').remove('searchControl').remove('typeSelector').remove('geolocationControl').remove('fullscreenControl').remove('rulerControl');
+}
